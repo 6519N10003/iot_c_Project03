67c14b721431b4984dd43936a67cc26d2d1952ca
S6519N10003 <s6519n10003@sau.ac.th>
2023-06-29 10:12:56 +0700

    version 1

---
+++ test01.c
@@ -0,0 +1,19 @@
+N#include <stdio.h>
+//git config --global user.name "S6519N10003"
+//git config --global user.email "s6519n10003@sau.ac.th"
+
+//git init
+//git add .
+//git commit -m "Version......"
+//git remote ...........
+
+//git push -u origin master
+
+void main ()
+{
+    printf ("IOT\n") ;
+    printf ("IOT\n") ;
+    printf ("IOT\n") ;
+    printf ("IOT\n") ;
+    printf ("IOT\n") ;
+}
